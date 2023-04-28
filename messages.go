5e70fb61246ea612519b0fa5299f57d1cf121d7e
mengb15 <mengb15@chinaunicom.cn>
2023-04-28 08:43:27 +0800

    feat(新增其它微信效率类型定义):

--- messages.go
+++ messages.go
@@ -2,6 +2,17 @@ package main
 
 import "encoding/xml"
 
+type WeChatCommon struct {
+	XMLName      xml.Name `xml:"xml"`
+	ToUserName   string
+	FromUserName string
+	CreateTime   int64
+	MsgType      string
+	MsgId        string
+	MsgDataId    string
+	Idx          string
+}
+
 // The WeChatMsg struct defines the structure for a WeChat message. It contains six fields:
 // - XMLName: This is the name of the XML element.
 // - ToUserName: The account ID of the recipient
@@ -10,12 +21,8 @@ import "encoding/xml"
 // - MsgType: The type of message. (text, image, voice etc.)
 // - Content: The message content
 type WeChatMsg struct {
-	XMLName      xml.Name `xml:"xml"`
-	ToUserName   string
-	FromUserName string
-	CreateTime   int64
-	MsgType      string
-	Content      string
+	WeChatCommon
+	Content string
 }
 
 // The WeChatEncryptRequest struct defines the sturcture for a encrypted WeChat message. It has three fields:
@@ -28,11 +35,49 @@ type WeChatEncryptRequest struct {
 	Encrypt    string
 }
 
-
 type WeChatEncryptResponse struct {
 	XMLName      xml.Name `xml:"xml"`
 	Encrypt      string
 	MsgSignature string
 	Timestamp    string
 	Nonce        string
+}
+
+type WeChatImg struct {
+	WeChatCommon
+	PicUrl  string
+	MediaId string
+}
+
+type WeChatVoice struct {
+	WeChatCommon
+	MediaId string
+	Format  string
+}
+
+type WeChatVideo struct {
+	WeChatCommon
+	MediaId      string
+	ThumbMediaId string
+}
+
+type WeChatShortVideo struct {
+	WeChatCommon
+	MediaId      string
+	ThumbMediaId string
+}
+
+type WeChatLocation struct {
+	WeChatCommon
+	Location_X string
+	Location_Y string
+	Scale      string
+	Label      string
+}
+
+type WeChatLink struct {
+	WeChatCommon
+	Title       string
+	Description string
+	Url         string
 }
